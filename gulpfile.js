4490acdc577ec5279c16dc7e999fd5f047c308d8
jason hoang <jason.hoang@itsoninc.com>
2016-10-25 01:52:47 -0700

    add autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,14 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var minifyCss = require('gulp-clean-css');
+var autoprefixer = require('gulp-autoprefixer');
 
 //declare the task
 gulp.task('default', function(done) {
   gulp.src('./src/stylesheets/main.scss')
     .pipe(sass())
     .pipe(minifyCss())
+    .pipe(autoprefixer())
     .pipe(gulp.dest('.'))
     .on('end', done);
 });
