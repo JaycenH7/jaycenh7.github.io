3c3e2e74bd5ca6f257b960d18dffcc6699e4817a
jason hoang <jason.hoang@itsoninc.com>
2016-10-25 01:52:54 -0700

    add watch and default tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
-//import the necessary gulp plugins
+// Dependencies
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var minifyCss = require('gulp-clean-css');
 var autoprefixer = require('gulp-autoprefixer');
 
-//declare the task
-gulp.task('default', function(done) {
+// Task: styles
+gulp.task('styles', function(done) {
   gulp.src('./src/stylesheets/main.scss')
     .pipe(sass())
     .pipe(minifyCss())
@@ -13,3 +13,11 @@ gulp.task('default', function(done) {
     .pipe(gulp.dest('.'))
     .on('end', done);
 });
+
+// Task: watch
+gulp.task('watch', function() {
+  gulp.watch('./src/stylesheets/*.scss', ['styles']);
+});
+
+// Task: default
+gulp.task('default', ['styles', 'watch'])
